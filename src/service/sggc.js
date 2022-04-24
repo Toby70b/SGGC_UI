e97fc9c8be22782a96034dd5b65a63975fff9a78
Toby <tobypl407@googlemail.com>
2022-04-24 12:32:40 +0100

    add environment configs from SGGC url

--- src/service/sggc.js
+++ src/service/sggc.js
@@ -1,8 +1,5 @@
 import {fetchFromApi, returnPostOptions} from "./index";
 
-export const SGGC_API_URI = "http://localhost:8080/api/sggc/"
-
-
 export const getCommonGamesBetweenUsers= (requestObj,onSuccess,onError) =>{
-    return fetchFromApi(SGGC_API_URI, returnPostOptions(requestObj), onSuccess, onError);
+    return fetchFromApi(process.env.REACT_APP_SGGC_URL, returnPostOptions(requestObj), onSuccess, onError);
 }
