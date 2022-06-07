4fa5dfd7cab914c1ca3a38083dbef8afccbfa37d
Toby <tobypl407@googlemail.com>
2022-06-07 19:16:16 +0100

    type changes

--- src/components/GroupGameSearchResultsPanel/TableColumnSearchFilter.tsx
+++ src/components/GroupGameSearchResultsPanel/TableColumnSearchFilter.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, {Ref, useRef} from 'react';
 import {SearchOutlined} from '@ant-design/icons';
-import {Button, Input} from 'antd';
+import {Button, Input, InputRef} from 'antd';
 import {ColumnType} from 'antd/lib/table';
 
 export default function TableColumnTextFilterConfig<T>(): ColumnType<T> {
-    const searchInputHolder: { current: Input | null } = {current: null};
-
+    const searchInput : Ref<InputRef> = useRef(null);
     return {
         filterDropdown: ({setSelectedKeys, selectedKeys, confirm, clearFilters}) => {
             return <div style={{padding: 8}}>
                 <Input
-                    ref={node => searchInputHolder.current = node}
+                    ref={searchInput}
                     placeholder={'Search'}
                     value={selectedKeys[0]}
                     onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
@@ -34,7 +33,7 @@ export default function TableColumnTextFilterConfig<T>(): ColumnType<T> {
         filterIcon: filtered => <SearchOutlined style={{color: filtered ? '#1890ff' : undefined}}/>,
         onFilterDropdownVisibleChange: (visible) => {
             if (visible) {
-                setTimeout(() => searchInputHolder.current?.select());
+                setTimeout(() => searchInput.current?.select());
             }
         },
     }
